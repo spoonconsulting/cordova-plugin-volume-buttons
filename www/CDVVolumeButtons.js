7ad5c001d8760286cf7388727a457f4a21cc68c0
zfir <zafir.heerah@spoonconsulting.com>
2022-09-22 10:56:09 +0400

    JS file link Network Information Plugin

--- www/CDVVolumeButtons.js
+++ www/CDVVolumeButtons.js
@@ -1,15 +1,37 @@
 var cordova = require('cordova');
 var exec = require('cordova/exec');
+var channel = require('cordova/channel');
 
-var volumeEventHandler = cordova.addWindowEventHandler('volumebuttonslistener');
-volumeEventHandler.onHasSubscribersChange = function() {
-	if (volumeEventHandler.numHandlers === 1) {
-		exec(function() {
-			cordova.fireWindowEvent('volumebuttonslistener', null);
-		}, function(e) {
-			console.log("CDVVolumeButtons error", e);
-		}, 'CDVVolumeButtons', 'start', []);
-	} else if (volumeEventHandler.numHandlers === 0) {
-		exec(null, null, 'CDVVolumeButtons', 'stop', []);
-	}
+var VolumeButtons = function() {
+	this.type = "unknown";
 }
+
+VolumeButtons.prototype.getInfo = function (successCallback, errorCallback) {
+	exec(successCallback, errorCallback, 'CDVVolumeButtons', 'start', []);
+};
+
+var volumeButtons = new VolumeButtons();
+var timerId = null;
+var timeout = 500;
+
+channel.createSticky('onCordovaConnectionReady');
+channel.waitForInitialization('onCordovaConnectionReady');
+
+channel.onCordovaReady.subscribe(function () {
+	volumeButtons.getInfo(function() {
+		timerId = setTimeout(function () {
+			cordova.fireDocumentEvent("volumebuttonslistener");
+		}, timeout);
+	}, function(e) {
+		if (timerId !== null) {
+			clearTimeout(timerId);
+			timerId = null;
+		}
+		console.log("CDVVolumeButtons error: " + e);
+	});
+	if (channel.onCordovaConnectionReady.state !== 2) {
+		channel.onCordovaConnectionReady.fire();
+	}
+});
+
+module.exports = volumeButtons;
