255c84fca4959c83a5ce081939f76c194c9f7e60
zfir <zafir.heerah@spoonconsulting.com>
2022-09-23 10:59:48 +0400

    Remove timeout, Add info

--- www/CDVVolumeButtons.js
+++ www/CDVVolumeButtons.js
@@ -16,22 +16,14 @@ VolumeButtons.prototype.getInfo = function (successCallback, errorCallback) {
 };
 
 var volumeButtons = new VolumeButtons();
-var timerId = null;
-var timeout = 500;
 
 channel.createSticky('onCordovaConnectionReady');
 channel.waitForInitialization('onCordovaConnectionReady');
 
 channel.onCordovaReady.subscribe(function () {
-	volumeButtons.getInfo(function() {
-		timerId = setTimeout(function () {
-			cordova.fireDocumentEvent("volumebuttonslistener");
-		}, timeout);
+	volumeButtons.getInfo(function(info) {
+        cordova.fireDocumentEvent("volumebuttonslistener", info)
 	}, function(e) {
-		if (timerId !== null) {
-			clearTimeout(timerId);
-			timerId = null;
-		}
 		console.log("CDVVolumeButtons error: " + e);
 	});
 	if (channel.onCordovaConnectionReady.state !== 2) {
