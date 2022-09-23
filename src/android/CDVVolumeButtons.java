010a8d3e4b620d66e2da4d3dcc75a915a98fc87e
zfir <zafir.heerah@spoonconsulting.com>
2022-09-23 18:42:40 +0400

    CDVVolumeButtons - iOS, Android V1

--- src/android/CDVVolumeButtons.java
+++ src/android/CDVVolumeButtons.java
@@ -1,32 +1,78 @@
 package com.spoon.volumebuttons;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.view.KeyEvent;
+import android.view.View;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
-/**
- * This class echoes a string called from JavaScript.
- */
-public class CDVVolumeButtons extends CordovaPlugin {
+public class CDVVolumeButtons extends CordovaPlugin implements View.OnKeyListener {
+    private CallbackContext volumeCallbackContext;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+        if (action.equals("start")) {
+            start(callbackContext);
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+            pluginResult.setKeepCallback(true);
+            this.volumeCallbackContext.sendPluginResult(pluginResult);
             return true;
+        } else if (action.equals("stop")) {
+            stop();
+            return false;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
+    @Override
+    public void onResume(boolean multitasking) {
+        this.webView.getView().setOnKeyListener(this);
+    }
+
+    @Override
+    public void onReset() {
+        this.webView.getView().setOnKeyListener(null);
+    }
+
+    @Override
+    public void onDestroy() {
+        this.webView.getView().setOnKeyListener(null);
+    }
+
+    private void start(CallbackContext callbackContext) {
+        if (this.volumeCallbackContext != null) {
+            return;
         }
+        this.volumeCallbackContext = callbackContext;
+        this.webView.getView().setOnKeyListener(this);
+    }
+
+    private void stop() {
+        this.webView.getView().setOnKeyListener(null);
+        this.volumeCallbackContext.success();
+        this.volumeCallbackContext = null;
+    }
+
+    @Override
+    public boolean onKey(View view, int i, KeyEvent keyEvent) {
+        if (keyEvent.getAction() == KeyEvent.ACTION_DOWN ) {
+            if(i == KeyEvent.KEYCODE_VOLUME_UP) {
+                PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+                pluginResult.setKeepCallback(true);
+                this.volumeCallbackContext.sendPluginResult(pluginResult);
+            }
+            else if (i == KeyEvent.KEYCODE_VOLUME_DOWN) {
+                PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+                pluginResult.setKeepCallback(true);
+                this.volumeCallbackContext.sendPluginResult(pluginResult);
+            }
+        }
+        return false;
     }
 }
