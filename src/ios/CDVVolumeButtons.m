3fa25567ace8be8a914ff7b6a53b64b4bcf76d51
zfir <zafir.heerah@spoonconsulting.com>
2022-10-10 11:28:07 +0400

    Fix setVolume, Use AVAudioSession

--- src/ios/CDVVolumeButtons.m
+++ src/ios/CDVVolumeButtons.m
@@ -18,14 +18,12 @@
 @implementation CDVVolumeButtons
 
 - (void)onPause {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.01 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        self.volumeViewSlider.value = self.defaultVolume;
-    });
     [self.volumeButtonHandler stopHandler];
 }
 
 - (void)onResume {
-    self.defaultVolume = self.volumeViewSlider.value;
+    AVAudioSession *avAudioSession = AVAudioSession.sharedInstance;
+    self.defaultVolume = avAudioSession.outputVolume;
     [self.volumeButtonHandler startHandler:YES];
 }
 
@@ -37,33 +35,27 @@
             break;
         }
     }
+    AVAudioSession *avAudioSession = AVAudioSession.sharedInstance;
+    self.defaultVolume = avAudioSession.outputVolume;
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onPause) name:UIApplicationDidEnterBackgroundNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onResume) name:UIApplicationWillEnterForegroundNotification object:nil];
     if (self.volumeButtonHandler == nil) {
         self.volumeButtonHandler = [JPSVolumeButtonHandler volumeButtonHandlerWithUpBlock:^{
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.01 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                self.volumeViewSlider.value = 0.9f;
-            });
+            self.volumeViewSlider.value = self.defaultVolume;
             CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
             [pluginResult setKeepCallback:@YES];
             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
         } downBlock:^{
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.01 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                self.volumeViewSlider.value = 0.1f;
-            });
+            self.volumeViewSlider.value = self.defaultVolume;
             CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
             [pluginResult setKeepCallback:@YES];
             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
         }];
     }
-    self.defaultVolume = self.volumeViewSlider.value;
     [self.volumeButtonHandler startHandler:YES];
 }
 
 - (void)stop:(CDVInvokedUrlCommand*)command {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.01 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        self.volumeViewSlider.value = self.defaultVolume;
-    });
     [self.volumeButtonHandler stopHandler];
 }
 
