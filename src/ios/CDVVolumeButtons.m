cb47a6061becd98b12b7bf2878e455c8cf6c073a
zfir <zafir.heerah@spoonconsulting.com>
2022-10-26 11:03:48 +0400

    Convert JPSVolumeButtonHandler to CDVVolumeButtonsController in CDVVolumeButtons

--- src/ios/CDVVolumeButtons.m
+++ src/ios/CDVVolumeButtons.m
@@ -1,6 +1,6 @@
 #import <Cordova/CDV.h>
 #import "MediaPlayer/MPVolumeView.h"
-#import <JPSVolumeButtonHandler.h>
+#import "CDVVolumeButtonsController.h"
 
 @interface CDVVolumeButtons : CDVPlugin {
   // Member variables go here.
@@ -10,7 +10,7 @@
 - (void)onResume;
 - (void)start:(CDVInvokedUrlCommand*)command;
 - (void)stop:(CDVInvokedUrlCommand*)command;
-@property (strong, nonatomic) JPSVolumeButtonHandler *volumeButtonHandler;
+@property (strong, nonatomic) CDVVolumeButtonsController *volumeButtonHandler;
 @end
 
 @implementation CDVVolumeButtons
@@ -36,7 +36,7 @@
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onPause) name:UIApplicationDidEnterBackgroundNotification object:nil];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onResume) name:UIApplicationWillEnterForegroundNotification object:nil];
         if (self.volumeButtonHandler == nil) {
-            self.volumeButtonHandler = [JPSVolumeButtonHandler volumeButtonHandlerWithUpBlock:^{
+            self.volumeButtonHandler = [CDVVolumeButtonsController volumeButtonHandlerWithUpBlock:^{
                 CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
                 [pluginResult setKeepCallback:@YES];
                 [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
